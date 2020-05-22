045577c0410c8f493a6d3702d8865358b77a2a91
Shanikan <62619626+Shanikan@users.noreply.github.com>
2020-05-22 19:06:50 -0400

    Create DGRoll.py
    Quick and Dirty Dice Roller because I got sick of a bot giving me bad rolls. Utterly pointless because of it, but I might customize it to fit the tabletop I play.

---
+++ DGRoll.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import random
+import argparse
+import sys
+
+# Argparse Setup
+
+parser = argparse.ArgumentParser(description="This program exists to roll dice.")
+parser.add_argument('-d', metavar='dice', type=int, nargs='+', help="The number of sides to be rolled")
+args = parser.parse_args()
+try:
+  usrinput = args.d[0]
+except:
+  print("Use the argument -h to show the help prompts.")
+  sys.exit()
+
+#def dice(type):
+result = random.randint(1,usrinput)
+
+#result = dice(usrinput)
+print(result)
